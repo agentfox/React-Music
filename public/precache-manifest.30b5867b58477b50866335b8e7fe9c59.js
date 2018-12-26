1688090c6c7b3d43157edd51c538c0cf81f91d80
cuong <caitenkhongbitrung@gmail.com>
2018-12-27 03:16:55 +0700

    ready to deploy

---
+++ public/precache-manifest.30b5867b58477b50866335b8e7fe9c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddaa2f67717275d7143b",
+    "url": "/static/js/main.ddaa2f67.chunk.js"
+  },
+  {
+    "revision": "a2640bbeb5d17936b742",
+    "url": "/static/js/1.a2640bbe.chunk.js"
+  },
+  {
+    "revision": "ddaa2f67717275d7143b",
+    "url": "/static/css/main.696e0d9c.chunk.css"
+  },
+  {
+    "revision": "a2640bbeb5d17936b742",
+    "url": "/static/css/1.36e039bf.chunk.css"
+  },
+  {
+    "revision": "800ca9aa04057e94e09560c81b2fd2c0",
+    "url": "/index.html"
+  }
+];
