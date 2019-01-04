4ef942e141e02dc388409d277cd26b8ccf7608cd
cuong <caitenkhongbitrung@gmail.com>
2019-01-04 19:54:41 +0700

    built client

---
+++ public/precache-manifest.a4e1157a0672d108094446a34c7345c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe4dc2fc6442bf2b021c4ae70991269d",
+    "url": "/static/media/play-button.fe4dc2fc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e4019b8a841183c59606",
+    "url": "/static/js/main.e4019b8a.chunk.js"
+  },
+  {
+    "revision": "e136126a12937eaf56e9",
+    "url": "/static/js/1.e136126a.chunk.js"
+  },
+  {
+    "revision": "e4019b8a841183c59606",
+    "url": "/static/css/main.1f876368.chunk.css"
+  },
+  {
+    "revision": "e136126a12937eaf56e9",
+    "url": "/static/css/1.29dbdf51.chunk.css"
+  },
+  {
+    "revision": "0b409ea84ffbd83e372eac9e5360dbdd",
+    "url": "/index.html"
+  }
+];
