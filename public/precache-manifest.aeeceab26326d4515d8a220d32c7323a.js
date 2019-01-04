4a7a9a224e4838e65583227618b9a75c283674cd
cuong <caitenkhongbitrung@gmail.com>
2019-01-04 20:03:18 +0700

    for got to set auto save , built client

---
+++ public/precache-manifest.aeeceab26326d4515d8a220d32c7323a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe4dc2fc6442bf2b021c4ae70991269d",
+    "url": "/static/media/play-button.fe4dc2fc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "190121508ff2d454c1a4",
+    "url": "/static/js/main.19012150.chunk.js"
+  },
+  {
+    "revision": "e136126a12937eaf56e9",
+    "url": "/static/js/1.e136126a.chunk.js"
+  },
+  {
+    "revision": "190121508ff2d454c1a4",
+    "url": "/static/css/main.1f876368.chunk.css"
+  },
+  {
+    "revision": "e136126a12937eaf56e9",
+    "url": "/static/css/1.29dbdf51.chunk.css"
+  },
+  {
+    "revision": "6222e63e2a7e9dc727a9c25f6ca68f52",
+    "url": "/index.html"
+  }
+];
